513f8a9a938b8b9c852e91212831987d3c6b26f6
alisonbeattie <alison.beattie@target.com>
2015-10-31 13:47:56 -0500

    first commit full

---
+++ README.md
@@ -0,0 +1,1 @@
+# MadWomen_v1
---
+++ grid.php
@@ -0,0 +1,107 @@
+        <!-- portfolio -->
+        <div class="row row-centered">
+          <div class="col-xs-12 col-centered teal-circle">
+              <i class="fa fa-chevron-down fa-1x" ></i>
+          </div>
+        </div>
+
+
+        <div class="container">
+          <div class="row row-centered">
+            <div class="row row-centered">
+              <div class="col-xs-12">
+                <h3>SELECTED WORK</h3>
+                <div class="sm-teal"></div>
+              </div>
+            </div>
+          </div>
+        </div> <!-- end portfolio row -->
+
+
+<div id="portfoliolist">
+              
+              <div class="col-xs-12 col-sm-6 col-md-4 portcol">
+                      <div style="cursor: pointer;" onclick="window.location='ninja.php';" class="portfolio logo">
+                        <div class="portfolio-wrapper">
+                          <img src="portimgs/thumbs/thumb_ninja.jpg" alt="" />
+                          <div class="portlabel">
+                            <div class="portlabel-text">
+                              <a class="text-title">Jill of all Trades</a>
+                              <span class="text-category">Ninja UXer</span>
+                            </div>
+                            <div class="portlabel-bg"></div>
+                          </div>
+                        </div>
+                      </div> 
+              </div>
+              <div class="col-xs-12 col-sm-6 col-md-4 portcol">
+                      <div style="cursor: pointer;" onclick="window.location='weementor.php';" class="portfolio logo">
+                        <div class="portfolio-wrapper">       
+                          <img src="portimgs/thumbs/thumb_startup.jpg" alt="" />
+                          <div class="portlabel">
+                            <div class="portlabel-text">
+                              <a class="text-title">Wee Mentor</a>
+                              <span class="text-category">Founder &amp; Designer</span>
+                            </div>
+                            <div class="portlabel-bg"></div>
+                          </div>
+                        </div>
+                      </div> 
+              </div> 
+              <div class="col-xs-12 col-sm-6 col-md-4 portcol">
+                      <div style="cursor: pointer;" onclick="window.location='arbys.php';" class="portfolio logo">
+                        <div class="portfolio-wrapper">       
+                          <img src="portimgs/thumbs/thumb_arbys.jpg" alt="" />
+                          <div class="portlabel">
+                            <div class="portlabel-text">
+                              <a class="text-title">Arby's</a>
+                              <span class="text-category">Experience Design</span>
+                            </div>
+                            <div class="portlabel-bg"></div>
+                          </div>
+                        </div>
+                      </div> 
+              </div> 
+              <div class="col-xs-12 col-sm-6 col-md-4 portcol">
+                      <div style="cursor: pointer;" onclick="window.location='starterkit.php';" class="portfolio logo">
+                        <div class="portfolio-wrapper">       
+                          <img src="portimgs/thumbs/thumb_starterkit.jpg" alt="" />
+                          <div class="portlabel">
+                            <div class="portlabel-text">
+                              <a class="text-title">Starter Kit</a>
+                              <span class="text-category">Designer</span>
+                            </div>
+                            <div class="portlabel-bg"></div>
+                          </div>
+                        </div>
+                      </div> 
+              </div> 
+              <div class="col-xs-12 col-sm-6 col-md-4 portcol">
+                      <div style="cursor: pointer;" onclick="window.location='cadillac.php';" class="portfolio logo">
+                        <div class="portfolio-wrapper">       
+                          <img src="portimgs/thumbs/thumb_cadillac.jpg" alt="" />
+                          <div class="portlabel">
+                            <div class="portlabel-text">
+                              <a class="text-title">Cadillac</a>
+                              <span class="text-category">UX Leadership</span>
+                            </div>
+                            <div class="portlabel-bg"></div>
+                          </div>
+                        </div>
+                      </div> 
+              </div> 
+              <div class="col-xs-12 col-sm-6 col-md-4 portcol">
+                      <div style="cursor: pointer;" onclick="window.location='deluxe.php';" class="portfolio logo">
+                        <div class="portfolio-wrapper">       
+                          <img src="portimgs/thumbs/thumb_deluxe.jpg" alt="" />
+                          <div class="portlabel">
+                            <div class="portlabel-text">
+                              <a class="text-title">Deluxe</a>
+                              <span class="text-category">UX Leadership</span>
+                            </div>
+                            <div class="portlabel-bg"></div>
+                          </div>
+                        </div>
+                      </div> 
+              </div>
+</div>
